5928ef69d54dfc0caeb3ec691379e075d73447e5
Daniel Zimpel Wayhs <danielzimpel@cwi.com.br>
2013-06-14 22:45:51 -0300

    Added rails bootstrap app

---
+++ mobile-stats/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileStats::Application.config.secret_token = 'ffcc35f6bb42f90139a61932cf06570aba20d209b8deb61940fee2c492d97578189b18e2a51acfc4b7ec7cb127d95cbcac344d40ef37cfacf378cc70cd57fd36'
