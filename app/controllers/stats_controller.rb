5ae5dcbea905a196c298e51c297e1b63c35839c7
Rafael Garbin <rafaelgarbin@cwi.com.br>
2013-06-17 11:26:44 -0300

    Adicionando uma validação simples no cookies para não permitir enviar duas vezes os dados do dispositivo.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -24,15 +24,18 @@ class StatsController < ApplicationController
   end
 
   def count
-    if request.post?
+    if request.post? 
       @stat_data = StatData.new(params[:device_data])
       @stat_data.event_name = params[:event_name]
       @stat_data.ip_address = request.remote_ip
 
-      if @stat_data.save
-        message = {message: 'Success'}
+	  if cookies[:sent_count_stats]
+		message = {message: 'duplicated'}
+      elsif @stat_data.save
+		cookies[:sent_count_stats] = true
+        message = {message: 'success'}
       else
-        message = {message: 'Fail'}
+        message = {message: 'fail'}
       end
 
       render :json => message
