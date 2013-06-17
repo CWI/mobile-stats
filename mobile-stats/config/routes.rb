1a329a2e15deff62c70588eaec269012910c879d
Daniel Zimpel Wayhs <danielzimpel@cwi.com.br>
2013-06-17 07:41:59 -0300

    Implemented basic backend for stats counting

--- mobile-stats/config/routes.rb
+++ mobile-stats/config/routes.rb
@@ -1,7 +1,7 @@
 MobileStats::Application.routes.draw do
   root :to => 'stats#index'
-  match ':id' => 'stats#home', :as => :stat_home
-  match ':id/report' => 'stats#report', :as => :report
-  match ':id/qrcode' => 'stats#qrcode', :as => :qrcode
-  match ':id/countme' => 'stats#count', :as => :count
+  match ':event_name/report' => 'stats#report', :as => :report
+  match ':event_name/report/data/:property' => 'stats#report_data', :as => :report_data
+  match ':event_name/qrcode' => 'stats#qrcode', :as => :qrcode
+  match ':event_name/countme' => 'stats#count', :as => :count
 end
