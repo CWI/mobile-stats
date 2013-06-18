6f28ecb987204c95c3a999e443a854a5092407db
Daniel Zimpel Wayhs <danielzimpel@cwi.com.br>
2013-06-18 07:10:45 -0300

    Implemented report parameter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 MobileStats::Application.routes.draw do
   root :to => 'stats#index'
-  match ':event_name/report' => 'stats#report', :as => :report
-  match ':event_name/report/data/:property' => 'stats#report_data', :as => :report_data
+  match ':event_name/report' => 'stats#report_redirect', :as => :report_redirect
+  match ':event_name/report/:property' => 'stats#report', :as => :report
+  match ':event_name/report/:property/data' => 'stats#report_data', :as => :report_data
   match ':event_name/qrcode' => 'stats#qrcode', :as => :qrcode
   match ':event_name/countme' => 'stats#count', :as => :count
 end
