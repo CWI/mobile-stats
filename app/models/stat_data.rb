efa7fd46931a4af93a5731836da6034ddd9c00ab
Daniel Zimpel Wayhs <danielzimpel@cwi.com.br>
2013-06-18 15:21:12 -0300

    Added all fields to attr_accessor

--- app/models/stat_data.rb
+++ app/models/stat_data.rb
@@ -1,4 +1,4 @@
 class StatData < ActiveRecord::Base
   attr_accessible :event_name, :ip_address, :user_agent, :platform, :browser, :vendor, :pixel_ratio, :resolution, :language, :property_value, :total_stats, :perc
-  attr_accessor :perc
+  attr_accessor :event_name, :ip_address, :user_agent, :platform, :browser, :vendor, :pixel_ratio, :resolution, :language, :property_value, :total_stats, :perc
 end
