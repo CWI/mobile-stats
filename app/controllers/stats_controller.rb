62cd512f2710829ccc0f3e5f823058639b79ca57
Rafael Garbin <rgarbin@gmail.com>
2013-06-21 23:10:02 -0300

    Fix #44 Clean code(code climate) - Method complexity StatsController::report_datacode climate

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -20,33 +20,26 @@ class StatsController < ApplicationController
   def report_data
     property = params[:property]
 
-    if (property == "android_version" or property == "ios_version")
-      # if don't have device version for android or iphone, then remove of query 
-      @stat_data = StatData.select("count(*) as total_stats, #{property} as property_value")
-                           .where('event_name = ?', params[:event_name])
-                           .where(property + " is not null")
-                           .where(property + " != ''")
-                           .group(property)
-                           .order('total_stats desc')
-    else
-      @stat_data = StatData.select("count(*) as total_stats, #{property} as property_value")
-                           .where('event_name = ?', params[:event_name])
-                           .group(property)
-                           .order('total_stats desc')
-    end
-
-    total_records = 0
-
-    @stat_data.each do |i|
-      total_records += Integer(i.total_stats)
-    end
+    @stat_data = StatData.select("  count(*) as total_stats 
+								  , #{property} as property_value")
+	   				      .where('event_name = ?', params[:event_name])
+					      .where(property + " is not null")
+					      .where(property + " != '' and ('#{property}' != 'android_version' or '#{property}' != 'ios_version')") 
+					      .group(property)
+					      .order('total_stats desc')
 
     @stat_data = @stat_data.collect do |i|
-      i.perc = ((i.total_stats.to_f / total_records.to_f) * 100.0).round
+      i.perc = ((i.total_stats.to_f / StatData.all.count.to_f) * 100.0).round
       i
     end
 
-    render :json => @stat_data.map { |stats| {:total_stats => Integer(stats.total_stats), :perc => stats.perc, :property_value => stats.property_value} }
+    render :json => @stat_data.map { |stats| 
+	{
+			:total_stats => Integer(stats.total_stats), 
+			:perc => stats.perc.to_f.round, 
+			:property_value => stats.property_value
+		} 
+	}
   end
 
   def qrcode
